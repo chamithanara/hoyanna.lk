9e280a36b6c6f13b1c26001970429f62cd6a0f17
chamithanara <chamithanara@gmail.com>
2015-12-11 16:39:14 +0530

    added login page and main view
    added login page and main view

---
+++ application/views/mainView.php
@@ -0,0 +1,264 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html lang="en" class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html lang="en" class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html lang="en" class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html lang="en" class="no-js"> <!--<![endif]-->
+<head>
+    <!-- Mobile Specific Meta -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Always force latest IE rendering engine -->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- Meta Keyword -->
+    <meta name="keywords" content="one page, business template, single page, onepage, responsive, parallax, creative, business, html5, css3, css3 animation">
+    <!-- meta character set -->
+    <meta charset="utf-8">
+
+    <!-- Site Title -->
+    <title>Hoyanna.lk</title>
+    
+    <!--
+    Google Fonts
+    ============================================= -->
+    <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700" rel="stylesheet" type="text/css">
+	
+    <!--
+    CSS
+    ============================================= -->
+    <!-- Fontawesome -->
+    <link rel="stylesheet" href="<?php echo base_url(); ?>/assets/css/font-awesome.min.css">
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="<?php echo base_url(); ?>/assets/css/bootstrap.min.css">
+    <!-- Fancybox -->
+    <link rel="stylesheet" href="<?php echo base_url(); ?>/assets/css/jquery.fancybox.css">
+    <!-- owl carousel -->
+    <link rel="stylesheet" href="<?php echo base_url(); ?>/assets/css/owl.carousel.css">
+    <!-- Animate -->
+    <link rel="stylesheet" href="<?php echo base_url(); ?>/assets/css/animate.css">
+    <!-- Main Stylesheet -->
+    <link rel="stylesheet" href="<?php echo base_url(); ?>/assets/css/main.css">
+    <!-- Main Responsive -->
+    <link rel="stylesheet" href="<?php echo base_url(); ?>/assets/css/responsive.css">
+	
+	
+	<!-- Modernizer Script for old Browsers -->
+    <script src="<?php echo base_url(); ?>/assets/js/vendor/modernizr-2.6.2.min.js"></script>
+	
+</head>
+
+<body>
+
+    <!--
+    Fixed Navigation
+    ==================================== -->
+    <header id="navigation" class="navbar-fixed-top animated-nav" >
+        <div class="container">
+
+            <div class="navbar-header">
+                <!-- responsive nav button -->
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <!-- /responsive nav button -->
+
+                <!-- logo -->
+                <h1 class="navbar-brand">
+                    <a href="#body">
+                        <img src="<?php echo base_url(); ?>/assets/img/logo.png" alt="Kasper Logo">
+                    </a>
+                </h1>
+                <!-- /logo -->
+
+                </div>
+
+                <!-- main nav -->
+                <nav class="collapse navigation navbar-collapse navbar-right" role="navigation">
+                    <ul id="nav" class="nav navbar-nav" style="color:black">
+                        <li class="current"><a href="#home">Home</a></li>
+                        <li><a style="color:black" href="#service">emploee Tab1</a></li>
+                        <li><a style="color:black" href="#about">emploee Tab2</a></li>
+                        <li><a style="color:black" href="#pricing">emploee Tab3</a></li>
+                        <li><a style="color:black" href="#contact">emploee Tab4</a></li>
+                    </ul>
+                </nav>
+                <!-- /main nav -->
+            </div>
+
+        </div>
+    </header>
+    <!--
+    End Fixed Navigation
+    ==================================== -->
+
+    <!--
+    #about-us
+    ========================== -->
+    <section id="about-us">
+        <div class="container">
+            <div class="row">
+                
+                <div class="col-md-5 col-md-offset-1 wow fadeInLeft">
+
+                    <div class="subtitle text-center">
+                        <h3>Testimonials</h3>
+                        <p>Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Mauris blandit aliquet elit, eget tincidunt.</p>
+                    </div>
+
+                    <div id="testimonial">
+
+                        <div class="tst-item clearfix">
+                            <div class="tst-single clearfix">
+                                <img src="<?php echo base_url(); ?>/assets/img/client/1.jpg" alt="Client" class="img-circle">
+                                <div class="tst-content">
+                                    <p>Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Mauris blandit aliquet elit, eget tincidunt.</p>
+                                    <span>John Doe,CEO</span>
+                                </div>
+                            </div>
+
+                            <div class="tst-single clearfix">
+                                <img src="<?php echo base_url(); ?>/assets/img/client/2.jpg" alt="Client" class="img-circle">
+                                <div class="tst-content">
+                                    <p>Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Mauris blandit aliquet elit, eget tincidunt.</p>
+                                    <span>John Doe,CEO</span>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="tst-item">
+                            <div class="tst-single clearfix">
+                                <img src="<?php echo base_url(); ?>/assets/img/client/3.jpg" alt="Client" class="img-circle">
+                                <div class="tst-content">
+                                    <p>Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Mauris blandit aliquet elit, eget tincidunt.</p>
+                                    <span>John Doe,CEO</span>
+                                </div>
+                            </div>
+                            <div class="tst-single clearfix">
+                                <img src="<?php echo base_url(); ?>/assets/mg/client/1.jpg" alt="Client" class="img-circle">
+                                <div class="tst-content">
+                                    <p>Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Mauris blandit aliquet elit, eget tincidunt.</p>
+                                    <span>John Doe,CEO</span>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="tst-item">
+                            <div class="tst-single clearfix">
+                                <img src="<?php echo base_url(); ?>/assets/img/client/2.jpg" alt="Client" class="img-circle">
+                                <div class="tst-content">
+                                    <p>Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Mauris blandit aliquet elit, eget tincidunt.</p>
+                                    <span>John Doe,CEO</span>
+                                </div>
+                            </div>
+                            <div class="tst-single clearfix">
+                                <img src="<?php echo base_url(); ?>/assets/img/client/3.jpg" alt="Client" class="img-circle">
+                                <div class="tst-content">
+                                    <p>Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Mauris blandit aliquet elit, eget tincidunt.</p>
+                                    <span>John Doe,CEO</span>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div> <!-- end #testimonial -->
+                </div> <!-- end .col-md-5 -->
+
+                <div class="col-md-5 col-md-offset-1 wow fadeInRight">
+
+                    <div class="subtitle text-center">
+                        <h3>OUR SKILLS</h3>
+                        <p>Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Mauris blandit aliquet elit, eget tincidunt.</p>
+                    </div>
+
+                    <div class="progress-bars">
+                        
+                        <span>HTML 5 - <small>95%</small></span>
+                        <div class="progress"  data-progress="95">
+                            <div class="bar"></div>
+                        </div>
+                        <span>CSS 3 - <small>93%</small></span>
+                        <div class="progress"  data-progress="93">
+                            <div class="bar"></div>
+                        </div>
+                        <span>PHP - <small>85%</small></span>
+                        <div class="progress" data-progress="85">
+                            <div class="bar"></div>
+                        </div>
+                        <span>mySQL - <small>87%</small></span>
+                        <div class="progress kill-margin"  data-progress="87">
+                            <div class="bar"></div>
+                        </div>
+
+                    </div>  <!-- progress-bars -->
+
+                </div>  <!-- end .col-md-5 -->
+
+            </div>
+        </div>
+    </section>
+    <!--
+    End #about-us
+    ========================== -->
+
+    <!--
+    #footer
+    ========================== -->
+    <footer id="footer" class="text-center">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+
+                    <div class="footer-logo wow fadeInDown">
+                        <img src="<?php echo base_url(); ?>/assets/img/logo.png" alt="logo">
+                    </div>
+
+                    <div class="footer-social wow fadeInUp">
+                        <h3>We are social</h3>
+                        <ul class="text-center list-inline">
+                            <li><a href="http://goo.gl/RqhEjP"><i class="fa fa-facebook fa-lg"></i></a></li>
+                            <li><a href="http://goo.gl/hUfpSB"><i class="fa fa-twitter fa-lg"></i></a></li>
+                            <li><a href="http://goo.gl/r4xzR4"><i class="fa fa-google-plus fa-lg"></i></a></li>
+                        </ul>
+                    </div>
+
+                    <div class="copyright">
+                        <p>Developed By Hoyanna.lk</p>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!--
+    End #footer
+    ========================== -->
+
+
+    <!--
+    JavaScripts
+    ========================== -->
+    
+    <!-- main jQuery -->
+    <script src="<?php echo base_url(); ?>/assets/js/vendor/jquery-1.11.1.min.js"></script>
+    <!-- Bootstrap -->
+    <script src="<?php echo base_url(); ?>/assets/js/bootstrap.min.js"></script>
+    <!-- jquery.nav -->
+    <script src="<?php echo base_url(); ?>/assets/js/jquery.nav.js"></script>
+    <!-- Portfolio Filtering -->
+    <script src="<?php echo base_url(); ?>/assets/js/jquery.mixitup.min.js"></script>
+    <!-- Fancybox -->
+    <script src="<?php echo base_url(); ?>/assets/js/jquery.fancybox.pack.js"></script>
+    <!-- Parallax sections -->
+    <script src="<?php echo base_url(); ?>/assets/js/jquery.parallax-1.1.3.js"></script>
+    <!-- jQuery Appear -->
+    <script src="<?php echo base_url(); ?>/assets/js/jquery.appear.js"></script>
+    <!-- countTo -->
+    <script src="<?php echo base_url(); ?>/assets/js/jquery-countTo.js"></script>
+    <!-- owl carousel -->
+    <script src="<?php echo base_url(); ?>/assets/js/owl.carousel.min.js"></script>
+    <!-- WOW script -->
+    <script src="<?php echo base_url(); ?>/assets/js/wow.min.js"></script>
+    <!-- theme custom scripts -->
+    <script src="<?php echo base_url(); ?>/assets/js/main.js"></script>
+</body>
+</html>
